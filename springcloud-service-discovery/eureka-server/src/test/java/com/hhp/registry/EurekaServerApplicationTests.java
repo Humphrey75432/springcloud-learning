b6489ab85c984346f1d5a81723fa4cad3ab7ad86
Humphrey Hu <humphrey_hu@163.com>
2022-02-19 17:01:24 +0800

    Spring Cloud: 新增服务注册和服务发现项目

---
+++ springcloud-service-discovery/eureka-server/src/test/java/com/hhp/registry/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhp.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
